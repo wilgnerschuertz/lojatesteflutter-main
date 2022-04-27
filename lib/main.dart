efc5c27c628a6a78b0d342c23472acbd263eb06a
wilgnerschuertz <60265271+wilgnerschuertz@users.noreply.github.com>
2022-04-26 23:25:22 -0400

    test: Firebase Hosting

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Projeto Fire',
+      title: 'Projeto',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
